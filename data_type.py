fb16712677e0ec0ad5f0328b776a08eba0a4ea0d
Muideen27 <ilorimuideen0000@gmail.com>
2025-06-07 18:02:56 +0100

    some exercise

--- data_type.py
+++ data_type.py
@@ -1,2 +1,16 @@
 ## We use the command touch to create a new file.
 
+funmi = "My name is Funmi."
+print(funmi)
+print(type(funmi))
+
+## check datatype of int, float, boalean, list, set, tuple, dic, 
+
+# To check for int
+
+num = 2 + 3
+result = num
+type = type(result)
+# print(result)
+print(f"The result is: {result}") # formating statment
+print(f"The type of the data is: {type}")
